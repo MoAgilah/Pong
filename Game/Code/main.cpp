5d81497553ece7180df57ee20b9896b96507dba1
Mo Agilah <32136453+MoAgilah@users.noreply.github.com>
2025-10-17 09:38:48 +0100

    Undo previous commit

--- Game/Code/main.cpp
+++ Game/Code/main.cpp
@@ -2,21 +2,8 @@
 #include <Engine/Core/Constants.h>
 #include "Utilities/ErrorUtils.h"
 
-#include <windows.h>
-#include <string>
-
-std::wstring getExePath() {
-	wchar_t buf[MAX_PATH];
-	DWORD n = GetModuleFileNameW(nullptr, buf, MAX_PATH);
-	return std::wstring(buf, n);
-}
-
-#include <filesystem>
-std::filesystem::path exeDir = std::filesystem::path(getExePath()).parent_path();
-
 int main()
 {
-	err::set_log_file(exeDir.string());
 	GameConstants::Init();
 	PongFramework framework;
 
